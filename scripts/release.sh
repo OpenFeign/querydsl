345439bd35475dc7a251f32d83d46919486f251b
Marvin Froeder <velo@users.noreply.github.com>
2023-11-06 21:14:40 +0000

    Poms sorted (#54)
    * Format poms
    
    * Apply sortPom to project
    
    * Remove external repositories
    
    * Temporaly disabled cache
    
    * Build all modules by default
    
    * Build all modules by default
    
    * Revert "Temporaly disabled cache"
    
    This reverts commit 3fd09f7391fcb55f7b41d3fdab062ff3db4d7a50.
    
    ---------
    
    Co-authored-by: Marvin Froeder <velobr@gmail.com>

--- scripts/release.sh
+++ scripts/release.sh
@@ -30,11 +30,11 @@ echo "release version is: ${tag} and next snapshot is: ${snapshot}"
 
 # Update the versions, removing the snapshots, then create a new tag for the release, this will
 # start the travis-ci release process.
-./mvnw -Pall,docs -B versions:set scm:checkin -DremoveSnapshot -DgenerateBackupPoms=false -Dmessage="prepare release ${tag}" -DpushChanges=false
+./mvnw -Pdocs -B versions:set scm:checkin -DremoveSnapshot -DgenerateBackupPoms=false -Dmessage="prepare release ${tag}" -DpushChanges=false
 
 # tag the release
 echo "pushing tag ${tag}"
-./mvnw -Pall,docs scm:tag
+./mvnw -Pdocs scm:tag
 
 # Update the versions to the next snapshot
-./mvnw -Pall,docs -B versions:set scm:checkin -DnewVersion="${snapshot}" -DgenerateBackupPoms=false -Dmessage="[ci skip] updating versions to next development iteration ${snapshot}"
+./mvnw -Pdocs -B versions:set scm:checkin -DnewVersion="${snapshot}" -DgenerateBackupPoms=false -Dmessage="[ci skip] updating versions to next development iteration ${snapshot}"
