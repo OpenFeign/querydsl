a6538053351150aad9d48c6f63094820b87851ce
Marvin Froeder <velobr@gmail.com>
2023-11-24 09:19:24 +1300

    Copy spring example from https://github.com/spring-guides/gs-accessing-data-jpa

---
+++ querydsl-examples/querydsl-example-jpa-spring/src/main/java/com/example/accessingdatajpa/CustomerRepository.java
@@ -0,0 +1,12 @@
+package com.example.accessingdatajpa;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+
+	List<Customer> findByLastName(String lastName);
+
+	Customer findById(long id);
+}
