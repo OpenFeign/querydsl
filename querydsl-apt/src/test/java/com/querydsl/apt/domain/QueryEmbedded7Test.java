730d70a7e9b924c2b037cf41c7c6700a09e6186d
Marvin Froeder <velobr@gmail.com>
2023-11-28 10:15:20 +1300

    Add missing import

--- querydsl-apt/src/test/java/com/querydsl/apt/domain/QueryEmbedded7Test.java
+++ querydsl-apt/src/test/java/com/querydsl/apt/domain/QueryEmbedded7Test.java
@@ -1,6 +1,6 @@
 package com.querydsl.apt.domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import com.querydsl.core.annotations.QueryEmbedded;
 import com.querydsl.core.annotations.QueryEntity;
@@ -10,7 +10,6 @@ import java.util.Collection;
 import java.util.Locale;
 import java.util.Set;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
 
 public class QueryEmbedded7Test {
 
@@ -30,8 +29,9 @@ public class QueryEmbedded7Test {
 
   @Test
   public void test() {
-    Assertions.assertEquals(
-        StringPath.class, QQueryEmbedded7Test_Entity.entity.users.any().getClass());
-    assertEquals(NumberPath.class, QQueryEmbedded7Test_Entity.entity.productRoles.any().getClass());
+    assertThat(QQueryEmbedded7Test_Entity.entity.users.any().getClass())
+        .isEqualTo(StringPath.class);
+    assertThat(QQueryEmbedded7Test_Entity.entity.productRoles.any().getClass())
+        .isEqualTo(NumberPath.class);
   }
 }
