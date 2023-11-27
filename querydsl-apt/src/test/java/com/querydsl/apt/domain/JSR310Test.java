d01d2ee60fd4294ab5c22f8f35708f0ac8990025
Marvin Froeder <velobr@gmail.com>
2023-11-28 10:15:20 +1300

    Add missing import

--- querydsl-apt/src/test/java/com/querydsl/apt/domain/JSR310Test.java
+++ querydsl-apt/src/test/java/com/querydsl/apt/domain/JSR310Test.java
@@ -1,6 +1,6 @@
 package com.querydsl.apt.domain;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Instant;
 import java.util.Date;
@@ -37,6 +37,6 @@ public class JSR310Test {
 
   @Test
   public void test() {
-    assertEquals(Date.class, QJSR310Test_BaseEntity.baseEntity.createdDate.getType());
+    assertThat(QJSR310Test_BaseEntity.baseEntity.createdDate.getType()).isEqualTo(Date.class);
   }
 }
