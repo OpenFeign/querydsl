b056851a3aa4e436059e11541726b4386b81a86d
Marvin Froeder <velobr@gmail.com>
2023-12-07 21:06:21 +1300

    Fix H2 tests

--- querydsl-jpa/src/test/java/com/querydsl/jpa/suites/H2EclipseLinkTest.java
+++ querydsl-jpa/src/test/java/com/querydsl/jpa/suites/H2EclipseLinkTest.java
@@ -9,10 +9,13 @@ import org.junit.experimental.categories.Category;
 @Category(H2.class)
 public class H2EclipseLinkTest extends AbstractJPASuite {
 
-  // public static class JPA extends JPABase { }
-  // public static class JPASQL extends JPASQLBase { }
-  // public static class JPAIntegration extends JPAIntegrationBase { }
-  // public static class Serialization extends SerializationBase { }
+  public static class JPA extends JPABase {}
+
+  public static class JPASQL extends JPASQLBase {}
+
+  public static class JPAIntegration extends JPAIntegrationBase {}
+
+  public static class Serialization extends SerializationBase {}
 
   @BeforeClass
   public static void setUp() throws Exception {
