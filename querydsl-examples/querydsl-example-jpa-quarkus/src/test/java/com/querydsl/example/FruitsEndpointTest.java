957d240143a6bc714874cceecdf0cec6cda724f1
Marvin Froeder <velo.br@gmail.com>
2024-08-05 09:57:15 -0300

    Latest best praticles for java projects

--- querydsl-examples/querydsl-example-jpa-quarkus/src/test/java/com/querydsl/example/FruitsEndpointTest.java
+++ querydsl-examples/querydsl-example-jpa-quarkus/src/test/java/com/querydsl/example/FruitsEndpointTest.java
@@ -2,7 +2,7 @@ package com.querydsl.example;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.Matchers.not;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
