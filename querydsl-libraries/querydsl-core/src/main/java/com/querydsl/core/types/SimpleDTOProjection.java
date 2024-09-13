34f846dae4edfb9b869d882b1d10cba3aeebe8b9
HyunJun Mun <137624597+Mouon@users.noreply.github.com>
2024-09-13 22:14:25 +0900

    feat : formatting

--- querydsl-libraries/querydsl-core/src/main/java/com/querydsl/core/types/SimpleDTOProjection.java
+++ querydsl-libraries/querydsl-core/src/main/java/com/querydsl/core/types/SimpleDTOProjection.java
@@ -1,25 +1,25 @@
 package com.querydsl.core.types;
 
 import com.querydsl.core.types.dsl.EntityPathBase;
-
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SimpleDTOProjection<T> extends FactoryExpressionBase<T> {
-    private final List<Expression<?>> expressions; // 일치하는 필드만 저장하기위한 리스트
-    private final Constructor<? extends T> constructor; // DTO의 생성자를 불러오기위한 것
+
+    private final List<Expression<?>> expressions;
+    private final Constructor<? extends T> constructor;
 
     public SimpleDTOProjection(Class<? extends T> type, EntityPathBase<?> entity) {
         super(type);
         this.expressions = generateExpressions(type, entity);
         this.constructor = findMatchingConstructor(type);
     }
-    // Expression은 QueryDSL에서 필드를 이렇게 선언함 Q클래스 그래서 Q클래스의 배열타입이 Expression이다.
+
     private List<Expression<?>> generateExpressions(Class<? extends T> dtoType, EntityPathBase<?> entity) {
         List<Expression<?>> expressions = new ArrayList<>();
-        for (Field field : dtoType.getDeclaredFields()) { //dtoType.getDeclaredFields() 해당 클래스에 직접 선언된 모든 필드(public, protected, default, private)를 반환
+        for (Field field : dtoType.getDeclaredFields()) {
             String fieldName = field.getName();
             try {
                 Field entityField = entity.getClass().getField(fieldName);
@@ -36,7 +36,6 @@ public class SimpleDTOProjection<T> extends FactoryExpressionBase<T> {
         return expressions;
     }
 
-    // 필드 이름이 일치하는 갯수만큼의 파라미터로 이루어진 생성자를 찾아서 리턴한다.
     private Constructor<? extends T> findMatchingConstructor(Class<? extends T> type) {
         for (Constructor<?> constructor : type.getDeclaredConstructors()) {
             if (constructor.getParameterTypes().length == expressions.size()) {
@@ -65,7 +64,6 @@ public class SimpleDTOProjection<T> extends FactoryExpressionBase<T> {
         return null;
     }
 
-    // 제네릭으로 호출 시점에 구체적인 타입 명
     public static <T> SimpleDTOProjection<T> fields(Class<? extends T> type, EntityPathBase<?> entity) {
         return new SimpleDTOProjection<>(type, entity);
     }
