b89c75b717f7ecb7cefcf5663d0c5a5f934fc953
Marvin Froeder <velobr@gmail.com>
2024-12-14 05:48:34 -0300

    Last stable build with hibernate-core beta-1
    Signed-off-by: Marvin Froeder <velobr@gmail.com>

--- querydsl-tooling/querydsl-apt/src/test/java/com/querydsl/apt/domain/Revision.java
+++ querydsl-tooling/querydsl-apt/src/test/java/com/querydsl/apt/domain/Revision.java
@@ -1,12 +1,12 @@
 package com.querydsl.apt.domain;
 
 import jakarta.persistence.Entity;
+import org.hibernate.envers.DefaultRevisionEntity;
 import org.hibernate.envers.RevisionEntity;
-import org.hibernate.envers.RevisionMapping;
 
 @Entity
 @RevisionEntity
-public class Revision extends RevisionMapping {
+public class Revision extends DefaultRevisionEntity {
 
   private static final long serialVersionUID = 4587663183059799464L;
 }
