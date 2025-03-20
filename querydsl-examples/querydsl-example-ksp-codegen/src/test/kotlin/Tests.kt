db0e79442c68aac6a000448d00cb40122244b57e
a3magic3pocket <a3magic3pocket@gmail.com>
2025-03-20 09:34:14 +0900

    Add missing imports for QPersonClassDTO and QPersonClassConstructorDTO.

--- querydsl-examples/querydsl-example-ksp-codegen/src/test/kotlin/Tests.kt
+++ querydsl-examples/querydsl-example-ksp-codegen/src/test/kotlin/Tests.kt
@@ -3,6 +3,8 @@ import com.querydsl.example.ksp.CatType
 import com.querydsl.example.ksp.Person
 import com.querydsl.example.ksp.QCat
 import com.querydsl.example.ksp.QPerson
+import com.querydsl.example.ksp.QPersonClassDTO
+import com.querydsl.example.ksp.QPersonClassConstructorDTO
 import com.querydsl.jpa.impl.JPAQueryFactory
 import jakarta.persistence.EntityManagerFactory
 import org.hibernate.cfg.AvailableSettings
