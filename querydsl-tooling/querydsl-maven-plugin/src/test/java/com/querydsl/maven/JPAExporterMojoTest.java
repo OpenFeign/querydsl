c1dacc3ef65122a8c2255880122185fdebd76769
Kamil Krzywański <kamilkrzywanski01@gmail.com>
2026-06-03 13:12:46 +0200

    Migration of maven module to junit5

--- querydsl-tooling/querydsl-maven-plugin/src/test/java/com/querydsl/maven/JPAExporterMojoTest.java
+++ querydsl-tooling/querydsl-maven-plugin/src/test/java/com/querydsl/maven/JPAExporterMojoTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import org.apache.maven.project.MavenProject;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class JPAExporterMojoTest {
 
